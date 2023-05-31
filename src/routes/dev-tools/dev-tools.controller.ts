eddd8bc067f484067251d3a7de8388be01135b53
Leander Paul <leanderpaul97@outlook.com>
2023-05-31 20:23:51 +0100

    feat: changed graphiql url path and added custom storage for each app

--- src/routes/dev-tools/dev-tools.controller.ts
+++ src/routes/dev-tools/dev-tools.controller.ts
@@ -1,7 +1,7 @@
 /**
  * Importing npm packages
  */
-import { Controller, Get, Query, Render, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Render, UseGuards } from '@nestjs/common';
 import moment from 'moment';
 
 /**
@@ -10,10 +10,17 @@ import moment from 'moment';
 import { AuthService } from '@app/modules/auth';
 import { DevGuard } from '@app/shared/guards';
 import { Context, Storage } from '@app/shared/services';
+import { AppError, ErrorCode } from '@app/shared/errors';
 
 /**
  * Defining types
  */
+
+interface AdminTemplatePayload {
+  url: string;
+  appName: string;
+  csrfToken?: string;
+}
 
 /**
  * Declaring the constants
@@ -23,18 +30,19 @@ import { Context, Storage } from '@app/shared/services';
 export class DevToolsController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('graphiql')
+  @Get('graphiql/:appName')
   @UseGuards(DevGuard(true))
   @Render('admin.hbs')
-  getGraphiQL(@Query('app') app: string): { url: string; csrfToken?: string } {
+  getGraphiQL(@Param('appName') appName: string): AdminTemplatePayload {
     const apps = Storage.get<string[]>('graphql', []);
-    const url = `/graphql/${apps.includes(app) ? app : 'admin'}`;
+    if (!apps.includes(appName)) throw new AppError(ErrorCode.R001);
 
+    const url = `/graphql/${appName}`;
     const user = Context.getCurrentUser();
-    if (!user?.admin) return { url };
+    if (!user?.admin) return { url, appName };
 
     const expireAt = moment().add(2, 'hour');
     const csrfToken = this.authService.generateCSRFToken(expireAt);
-    return { csrfToken, url };
+    return { csrfToken, url, appName };
   }
 }
